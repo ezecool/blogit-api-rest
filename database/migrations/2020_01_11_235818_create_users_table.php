794c91e604f60968cb465e6ea6e451300becc985
Hernan Castro <castroabreguhernan@gmail.com>
2020-01-13 10:01:26 -0300

    migracion usuarios

--- database/migrations/2020_01_11_235818_create_users_table.php
+++ database/migrations/2020_01_11_235818_create_users_table.php
@@ -15,6 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->string('api_token', 60)->unique();
             $table->timestamps();
         });
     }
