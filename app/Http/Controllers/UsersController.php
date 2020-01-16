9610a8e2093004622953824af9470c07d1ae0b16
Hernan Castro <castroabreguhernan@gmail.com>
2020-01-16 12:40:44 -0300

    Funciona registro y Login

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,11 +11,6 @@ class UsersController extends Controller
 {
     function index(Request $request) {
 
-        // return 'Usuarios';
-        // $usuario = new User();
-        // $usuario->name = 'Hernan';
-        // $usuario->email = 'hernan@email.com';
-
       if ($request->isJson()) {
         $users = User::all();
         return response()->json($users, 200);
@@ -26,17 +21,9 @@ class UsersController extends Controller
 
     function createUser(Request $peticion) {
 
-      //$usuario = User::create([
-      //  'name' => 'Juan Perez',
-      //  'username' => 'jpere',
-      //  'email' => 'jperez@empresa.com',
-      //  'password' => Hash::make('123456'),
-      //  'api_token' => Str::random(60)
-      //]);
-
-      //return response()->json([], 201);
-
-      if ($peticion->isJson()) {
+      if (!$peticion->isJson()) {
+          return response()->json(['error' => 'No autorizado'], 401, []);
+      } else {
 
         // Tomo todos los datos de la peticion
         $data = $peticion->json()->all();
@@ -50,11 +37,8 @@ class UsersController extends Controller
         ]);
 
         // Si el recurso se creo exitosamente, devuelvo el usuario en formato json y el coddigo 201 (recurso creado)
-        return response()->json($usuario, 201);
-
+        return response()->json($data, 201);
       }
-
-      return response()->json(['error' => 'No autorizado'], 401, []);
     }
 
     public function getUser(Request $request, $id) {
@@ -73,23 +57,27 @@ class UsersController extends Controller
     }
 
     function getToken(Request $peticion) {
+
       if (!$peticion->isJson()) {
         return response()->json(['error' => 'No autorizado'], 401, []);
       } else {
         try {
           $data = $peticion->json()->all();
 
-          // Uso el metodo where del modelo User para buscar si existe un usuario con el username proporcionado
-          $usuario = User::where('username', $data['username'])->first();
+          // Uso el metodo where del modelo User para buscar si existe un usuario con el email proporcionado
+          $usuario = User::where('email', $data['email'])->first();
+
           if ($usuario && Hash::check($data['password'], $usuario->password)) {
+            // Devuelvo el usuario completo, incluido el api_token
             return response()->json($usuario, 200);
           } else {
-                return response()->json(['error' => 'Acceso incorrecto'], 406);
+            return response()->json(['error' => 'Acceso incorrecto'], 406);
           }
         } catch (ModelNotFoundException $e) {
             return response()->json(['error' => 'Acceso incorrecto'], 406);
         }
       }
+
     }
 
 }
